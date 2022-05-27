35c3c0fafcd8142556c5cde97256a0c0b61883e0
amantaya <aantaya@email.arizona.edu>
2022-05-27 17:55:39 +0000

    added function to clean Excel dates

--- scripts/functions.R
+++ scripts/functions.R
@@ -28,3 +28,29 @@ convert_from_excel_dates_to_datetime <- function(df, col_name){
     return(df)
   }
 }
+
+#' **Input:** A column named "Date.Entered" from a data frame load 
+#' from the Excel file, which was exported from Access database.
+#' 
+#' **What It Does:** It checks if the data frame has a "Date.Entered" column
+#' and if it does have that column, it converts the "Date.Entered" column
+#' from an Excel date to a S3 class datetime. If the data frame does not have a
+#' "Date.Entered" column, the functions returns nothing.
+#' 
+#' **Output:** It returns a date frame with the "Date.Entered" column converted \
+#' from a Excel date to date in the S3 date class.
+
+rename_all_docno_columns <- function(df, col_name){
+  # first check if the data frame has a "Date.Entered" column
+  if (any(names(df) == col_name)) {
+    # select only the "Date.Entered" column and pipe that into openxlsx to convert to correct date format for R
+    df[col_name] <- df$col_name %>% dplyr::rename(DocNo = col_name)
+    # return the cleaned data frame  
+    return(df)
+  }
+  else {
+    return(df)
+  }
+}
+
+
